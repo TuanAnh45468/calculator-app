be08c0b6797f01f41c9dbf4c4ba2d48c7d61e025
tuana <tuananh45468@gmail.com>
2023-05-10 22:41:30 +0700

    complete calculation function except DEL

--- dist/main.js
+++ dist/main.js
@@ -7,6 +7,8 @@ const toggle = document.querySelector(".toggle1");
 const header = document.querySelector(".header1");
 const resultSection = document.querySelector(".resultSection1");
 const calculation = document.querySelector(".calculation1");
+const delBtn = document.getElementById("delBtn");
+const resetBtn = document.getElementById("resetBtn");
 
 function updateTheme(themeValue) {
   const themeSuffixes = ["2", "3"];
@@ -35,4 +37,93 @@ function updateTheme(themeValue) {
 }
 themeSwitch.addEventListener("input", () => {
   updateTheme(themeSwitch.value);
+});
+
+let currentNumber = "";
+let nextNumber = "";
+let operatorArr = ["+", "-", "x", "/"];
+let operator = null;
+let result;
+
+function performCalculation() {
+  console.log("perform current", currentNumber);
+  console.log("perform next", nextNumber);
+  console.log("operator:", operator);
+  switch (operator) {
+    case "+":
+      result = Number(currentNumber) + Number(nextNumber);
+      break;
+    case "-":
+      result = Number(currentNumber) - Number(nextNumber);
+      break;
+    case "x":
+      result = Number(currentNumber) * Number(nextNumber);
+      break;
+    case "/":
+      if (nextNumber === "0") {
+        alert("Cannot divide by zero");
+        return;
+      }
+      result = Number(currentNumber) / Number(nextNumber);
+      break;
+    default:
+      return;
+  }
+
+  result = String(result);
+}
+
+function updateDisplay() {
+  const resultSection = document.querySelector(".resultSection1");
+  resultSection.textContent = `${currentNumber} ${
+    operator || ""
+  } ${nextNumber}`;
+}
+
+function displayResult() {
+  resultSection.textContent = result;
+}
+
+for (let btn of numberBtns) {
+  btn.addEventListener("click", (event) => {
+    const value = event.target.textContent;
+    console.log(value);
+    if (!operatorArr.includes(value) && !operator) {
+      currentNumber += value;
+    } else if (operatorArr.includes(value)) {
+      operator = value;
+    } else if (operator && value !== "=") {
+      nextNumber += value;
+    }
+    updateDisplay();
+  });
+}
+const operatorBtns = document.querySelectorAll(".operatorBtn");
+for (let operatorBtn of operatorBtns) {
+  operatorBtn.addEventListener("click", (event) => {
+    if (currentNumber && nextNumber) {
+      performCalculation();
+      currentNumber = result;
+      nextNumber = "";
+    }
+    operator = event.target.textContent;
+    updateDisplay();
+  });
+}
+
+equalBtn.addEventListener("click", () => {
+  if (currentNumber && nextNumber) {
+    performCalculation();
+    displayResult();
+    currentNumber = result;
+    nextNumber = "";
+    operator = null;
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  currentNumber = "";
+  nextNumber = "";
+  operator = null;
+  updateDisplay();
 });
