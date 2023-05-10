e9973260db4580f5a3c8873ca3be2ed813487b7b
tuana <tuananh45468@gmail.com>
2023-05-10 15:51:41 +0700

    clean up code

--- dist/main.js
+++ dist/main.js
@@ -1,80 +1,38 @@
 const themeSwitch = document.getElementById("theme-switch");
-const delResetBtn1 = document.getElementsByClassName("delResetBtn1");
-const numberBtn1 = document.getElementsByClassName("numberBtn1");
-const equalBtn1 = document.querySelector(".equalBtn1");
-const body1 = document.querySelector(".body1");
-const toggle1 = document.querySelector(".toggle1");
-const header1 = document.querySelector(".header1");
-const resultSection1 = document.querySelector(".resultSection1");
-const calculation1 = document.querySelector(".calculation1");
+const delResetBtns = document.getElementsByClassName("delResetBtn1");
+const numberBtns = document.getElementsByClassName("numberBtn1");
+const equalBtn = document.querySelector(".equalBtn1");
+const body = document.querySelector(".body1");
+const toggle = document.querySelector(".toggle1");
+const header = document.querySelector(".header1");
+const resultSection = document.querySelector(".resultSection1");
+const calculation = document.querySelector(".calculation1");
 
+function updateTheme(themeValue) {
+  const themeSuffixes = ["2", "3"];
+  const currentThemeSuffix = themeSuffixes.includes(themeValue)
+    ? themeValue
+    : "";
+
+  for (let suffix of themeSuffixes) {
+    const isCurrentTheme = suffix === currentThemeSuffix;
+
+    for (let btn of delResetBtns) {
+      btn.classList.toggle(`delResetBtn${suffix}`, isCurrentTheme);
+    }
+
+    for (let btn of numberBtns) {
+      btn.classList.toggle(`numberBtn${suffix}`, isCurrentTheme);
+    }
+
+    body.classList.toggle(`body${suffix}`, isCurrentTheme);
+    toggle.classList.toggle(`toggle${suffix}`, isCurrentTheme);
+    equalBtn.classList.toggle(`equalBtn${suffix}`, isCurrentTheme);
+    header.classList.toggle(`header${suffix}`, isCurrentTheme);
+    resultSection.classList.toggle(`resultSection${suffix}`, isCurrentTheme);
+    calculation.classList.toggle(`calculation${suffix}`, isCurrentTheme);
+  }
+}
 themeSwitch.addEventListener("input", () => {
-  const themeValue = themeSwitch.value;
-  if (themeValue === "2") {
-    for (let delResetBtn1Element of delResetBtn1) {
-      delResetBtn1Element.classList.remove("delResetBtn3");
-      delResetBtn1Element.classList.add("delResetBtn2");
-    }
-
-    for (let numberBtn1Element of numberBtn1) {
-      numberBtn1Element.classList.remove("numberBtn3");
-      numberBtn1Element.classList.add("numberBtn2");
-    }
-
-    body1.classList.add("body2");
-    body1.classList.remove("body3");
-    toggle1.classList.add("toggle2");
-    toggle1.classList.remove("toggle3");
-    equalBtn1.classList.add("equalBtn2");
-    equalBtn1.classList.remove("equalBtn3");
-    header1.classList.add("header2");
-    header1.classList.remove("header3");
-    resultSection1.classList.add("resultSection2");
-    resultSection1.classList.remove("resultSection3");
-    calculation1.classList.add("calculation2");
-    calculation1.classList.remove("calculation3");
-  }
-
-  if (themeValue === "3") {
-    for (let delResetBtn1Element of delResetBtn1) {
-      delResetBtn1Element.classList.add("delResetBtn3");
-    }
-
-    for (let numberBtn1Element of numberBtn1) {
-      numberBtn1Element.classList.add("numberBtn3");
-      numberBtn1Element.classList.remove("numberBtn2");
-    }
-
-    body1.classList.add("body3");
-    toggle1.classList.add("toggle3");
-    equalBtn1.classList.add("equalBtn3");
-    header1.classList.add("header3");
-    resultSection1.classList.add("resultSection3");
-    calculation1.classList.add("calculation3");
-  }
-
-  if (themeValue === "1") {
-    for (let delResetBtn1Element of delResetBtn1) {
-      delResetBtn1Element.classList.remove("delResetBtn2");
-      delResetBtn1Element.classList.remove("delResetBtn3");
-    }
-
-    for (let numberBtn1Element of numberBtn1) {
-      numberBtn1Element.classList.remove("numberBtn2");
-      numberBtn1Element.classList.remove("numberBtn3");
-    }
-
-    body1.classList.remove("body2");
-    body1.classList.remove("body3");
-    equalBtn1.classList.remove("equalBtn2");
-    equalBtn1.classList.remove("equalBtn3");
-    header1.classList.remove("header2");
-    header1.classList.remove("header3");
-    resultSection1.classList.remove("resultSection2");
-    resultSection1.classList.remove("resultSection3");
-    calculation1.classList.remove("calculation2");
-    calculation1.classList.remove("calculation3");
-    toggle1.classList.remove("toggle2");
-    toggle1.classList.remove("toggle3");
-  }
+  updateTheme(themeSwitch.value);
 });
